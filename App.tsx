1e7ca1f850c739f82bb8aac17755db38dd33541c
Lubnnia <lubnnia@gmail.com>
2024-12-01 14:41:09 -0300

    chore: Add style property in app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <Text>React</Text>
-      <Text>Native</Text>
-    </>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Nome do evento
+      </Text>
+      <Text
+        style={{
+          color: '#6B6B6B',
+          fontSize: 16,
+        }}
+      >
+        Segunda, 2 de Dezembro de 2024.
+      </Text>
+    </View>
   );
 }
