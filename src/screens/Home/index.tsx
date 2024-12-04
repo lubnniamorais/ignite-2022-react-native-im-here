4e770329d01c54987c0b4a82a43dec8f95195723
Lubnnia <lubnnia@gmail.com>
2024-12-04 11:39:51 -0300

    chore: Update structure at the project

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Segunda, 2 de Dezembro de 2024.</Text>
+    </View>
+  );
+}
